94b8136d3cf6716c7313250a15e8781d69b2a6b5
lambtron <andyjiang@gmail.com>
2022-03-16 18:00:41 -0700

    Commented out unnecessary files

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,3 +1,4 @@
-export default (req, res) => {
-  res.status(200).json({ text: "Hello" });
-};
+// Commenting this entire thing out, because we don't need it.
+// export default (req, res) => {
+//   res.status(200).json({ text: "Hello" });
+// };
